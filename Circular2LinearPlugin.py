c96c21bb48ab7ceae12ab738dd091edcb3382285
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-11-16 15:34:51 -0500

    Fixed bugs in DNA reverse

--- Circular2LinearPlugin.py
+++ Circular2LinearPlugin.py
@@ -1,5 +1,15 @@
 import PyPluMA
 
+def complement(DNA):
+    DNA_rev = DNA
+    DNA_rev = DNA_rev.replace('A', 'Q')
+    DNA_rev = DNA_rev.replace('T', 'A')
+    DNA_rev = DNA_rev.replace('Q', 'T')
+    DNA_rev = DNA_rev.replace('C', 'Z')
+    DNA_rev = DNA_rev.replace('G', 'C')
+    DNA_rev = DNA_rev.replace('Z', 'G')
+    return DNA_rev
+
 class Circular2LinearPlugin:
     def input(self, filename):
        params = open(filename, 'r')
@@ -9,6 +19,10 @@ class Circular2LinearPlugin:
           self.parameters[contents[0]] = contents[1]
        self.fasta = PyPluMA.prefix()+"/"+self.parameters["fasta"]
        self.start = int(self.parameters["start"])  #Assuming indexing is from 1
+       if ("reverse" in self.parameters and self.parameters["reverse"] == "true"):
+           self.reverse = True
+       else:
+           self.reverse = False
 
     def run(self):
        fastafile = open(self.fasta, 'r')
@@ -16,12 +30,21 @@ class Circular2LinearPlugin:
        DNA = ''
        for line in fastafile:
            DNA += line.strip()
-       print(DNA[9390:9993])
-       firstpart = DNA[self.start-1:]
-       secondpart = DNA[:self.start-1]
-       self.linDNA = firstpart+secondpart
-       print(len(self.linDNA))
-
+       print(len(DNA))
+       if (not self.reverse):
+          print("NOT REVERSING")
+          firstpart = DNA[self.start-1:]
+          secondpart = DNA[:self.start-1]
+          self.linDNA = firstpart+secondpart
+       else:
+           print("REVERSING")
+           firstpart = DNA[:self.start][::-1]
+           secondpart = DNA[self.start:][::-1]
+           self.linDNA = firstpart+secondpart
+           self.linDNA = complement(self.linDNA)
+           print(self.linDNA[482:2027])
+       #print(len(self.linDNA))
+       #print(self.linDNA[:20])
 
     def output(self, filename):
        outfile = open(filename, 'w')
